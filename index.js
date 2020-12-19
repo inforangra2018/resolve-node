652f9c45d4320b951c4adb84c76ed8febb5a2668
inforangra2018 <sandro@inforangra.com.br>
2020-12-19 02:33:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const { run } = require('micro')
+const { parse } = require('url')
+const fetch = require('node-fetch')
+const { maxSatisfying } = require('semver')
+
+const INDEX = 'https://nodejs.org/dist/index.json'
+
+async function resolveVersion(tag, filters = {}) {
+  const res = await fetch(INDEX)
+
+  if (!res.ok) {
+    // TODO: handle error response
+  }
+
+  let body = await res.json()
+
+  const ltsParts = tag.match(/^lts(?:\/([a-z]+))?$/)
+  const isLts = ltsParts !== null
+  if (isLts) {
+    const codename = ltsParts[1]
+    body = body.filter(b => b.lts && (codename ? b.lts.toLowerCase() === codename : true))
+  }
+
+  if (filters.security) {
+    body = body.filter(b => b.security)
+  }
+
+  const data = new Map(body.map(b => [b.version, b]))
+  const versions = body.map(b => b.version)
+  const matchTag = isLts ? '*' : tag
+  const version = maxSatisfying(versions, matchTag)
+  if (!version) {
+    return null
+  }
+  return Object.assign({ tag }, data.get(version))
+}
+
+async function handler (req, res) {
+  const { pathname, query } = parse(req.url, true)
+  const tag = (
+    query.tag ||
+    decodeURIComponent(pathname.substr(1)) ||
+    '*'
+  ).toLowerCase()
+  const onlyIncludeSecurityReleases = query.security === 'true'
+  const match = await resolveVersion(tag, {security: onlyIncludeSecurityReleases})
+
+  if (!match) {
+    res.statusCode = 404
+    return {
+      tag,
+      error: 'No match found'
+    }
+  }
+
+  if (/json/.test(req.headers.accept)) {
+    return match
+  } else {
+    res.setHeader('Content-Type', 'text/plain')
+    return match.version
+  }
+}
+
+module.exports = (req, res) => run(req, res, handler)
